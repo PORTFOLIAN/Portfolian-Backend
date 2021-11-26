071346a2825cee65387aec81a3a01ef28a4f2788
HONGMINJU <mhong0800@gmail.com>
2021-11-26 16:05:11 +0900

    feat : createProject 유효성 검사 추가

--- controllers/project.js
+++ controllers/project.js
@@ -13,12 +13,20 @@ let createProjectAritcle = async function(req,res){
   // owner 찾기 => 수정 필요
   const owner = await userServiceInstance.findUserByNickName(req.query.userId);
 
+  // 모집글 내용 유효성 확인
+  let validate = projectServiceInstance.validateArticleContents(req.body.article);
+  if (validate.code <= 0)
+  {
+    res.json(validate);
+    return;
+  }
+
   // project create & team에 findUser추가(역할 설정)
   const newProjectId = await projectServiceInstance.createProject(owner, req.body.article, req.body.ownerStack);
   
   //owner의 진행중인 프로젝트에 push
   await userServiceInstance.addDoingProject(owner, newProjectId);
-  res.json({newProjectID : newProjectId});
+  res.json({code : 1, message: "성공적으로 수행되었습니다.", newProjectID : newProjectId});
 }
 
 let modifyProjectAritcle = async function(req,res){
--- services/project.js
+++ services/project.js
@@ -20,5 +20,27 @@ class ProjectService{
         //this.ProjectModel.modifyProjectArticle(projectId, articleDto, ownweStack); 
         return findPro;
     }
+
+    validateArticleContents(articleDto){
+        if (!articleDto.title)
+            return {code : -1, message : "title 정보를 입력해주세요."};
+        else if (!articleDto.stackList)
+            return {code : -2, message : "stackList 정보를 입력해주세요."};
+        else if (articleDto.stackList.length == 0)
+            return {code : -3, message : "stackList는 빈 배열이 될 수 없습니다."};
+        else if (!articleDto.subjectDescription)
+            return {code : -4, message : "subjectDescription 정보를 입력해주세요."};
+        else if (!articleDto.projectTime)
+            return {code : -5, message : "projectTime 정보를 입력해주세요."};
+        else if (!articleDto.condition)
+            return {code : -6, message : "condition 정보를 입력해주세요."};
+        else if (!articleDto.progress)
+            return {code : -7, message : "progress 정보를 입력해주세요."};
+        else if (!articleDto.capacity)
+            return {code : -8, message : "capacity 정보를 입력해주세요."};
+        else if (articleDto.capacity <= 0)
+            return {code : -9, message : "capacity는 0이상 정수로 입력해주세요."};
+        return {code : 1, message : "validation 통과"};
+    }
 }
 module.exports  = ProjectService;
