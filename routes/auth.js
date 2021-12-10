945c0ad8ff5ec0496c566dd247dde4c27bb13a6c
HONGMINJU <mhong0800@gmail.com>
2021-12-10 17:45:34 +0900

    fix : req변수 읽는 방법 변경2

--- routes/auth.js
+++ routes/auth.js
@@ -17,6 +17,7 @@ const getUserInfo = async (access_token) => {
 
 
 router.post('/:coperation/access', async (req, res) => {
+  console.log("body : ", req.body);
   console.log("token : ", req.body.token);
   console.log("token[accessToken] : ", req.body.token[accessToken]);
   const userInfo = await getUserInfo(req.body.token.accessToken);
