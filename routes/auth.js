5a561a63410b605edd746ca643647f45ce448289
HONGMINJU <mhong0800@gmail.com>
2021-12-11 17:26:44 +0900

    fix : node-fetch => node-fetch-commonjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const fetch = require("node-fetch");
+const fetch = require('node-fetch-commonjs');
 
 const getUserInfo = async (access_token) => {
   try {
