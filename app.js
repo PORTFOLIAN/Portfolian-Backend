29b82c3cb3d2d7eaf70601704fbb7e3402bb1933
HONGMINJU <mhong0800@gmail.com>
2021-12-23 03:30:21 +0900

    feat : credentials 옵션 추가

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const Project = require('./models/project');
 const {  MONGO_URI } = process.env;
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials : true
+}));
 app.use(express.json());
 // app.use(express.urlencoded({ extends: true}));
 
