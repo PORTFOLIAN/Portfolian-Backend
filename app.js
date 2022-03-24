b12ea7afe04ec149539ff864f52725d349964ec5
HONGMINJU <mhong0800@gmail.com>
2022-03-24 17:27:10 +0900

    fix : socket.io 추가

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const loaders = require('./loaders/index.js');
 const express = require('express');
-
+const socketio = require('socket.io');
 const app = express();
 loaders(app);
 
@@ -14,7 +14,7 @@ const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/' + hostName + '/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/' + hostName + '/cert.pem'),
 };
-https.createServer(options, app).listen(443, () => {
+const server = https.createServer(options, app).listen(443, () => {
     console.log('443:번 포트에서 대기중입니다.');
 });
 
@@ -22,5 +22,27 @@ https.createServer(options, app).listen(443, () => {
 // const server = app.listen(3000, () => {
 //     console.log('Start Server : localhost:3000');
 // });
+
+const whiteList = ['http://3.35.89.48:3000','http://localhost:3000','http://portfolian.site:3000','https://portfolian.site:443','https://portfolian.site','https://3.35.89.48'];
+const io = socketio(server, { path: '/socket.io',  cors: { origin: whiteList } });
+io.on('connection',function(socket) {
+    console.log(`Connection : SocketId = ${socket.id}`);
+
+    socket.on('send', function(data) {
+
+        console.log('send message client to server')
+
+        const message_data = JSON.parse(JSON.stringify(data))
+        const messageContent = message_data.messageContent;
+        const roomId = message_data.roomId;
+
+        console.log(`roomId : ${roomId} message : ${messageContent}`);
+        io.emit('receive', { "messageContent" : messageContent })
+    });
+
+    socket.on('disconnect', function () {
+        console.log("One of sockets disconnected from our server.")
+    });
+})
 
 module.exports = app;
