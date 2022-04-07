0894b311d538d7bc01ffa145d4d2838d78fc2ddf
HONGMINJU <mhong0800@gmail.com>
2022-04-07 15:33:34 +0900

    fix : validateArticleContents 내 변수 선언

--- middlewares/validateArticleContents.js
+++ middlewares/validateArticleContents.js
@@ -1,22 +1,22 @@
 let validateArticleContents = async function(req, res, next) {
-    req
+    let articleDto = req.body.article;
     if (!articleDto.title)
         return res.status(200).json({code : -51, message : "title 정보를 입력해주세요."});
-    else if (!req.body.article.stackList)
+    else if (!articleDto.stackList)
         return res.status(200).json({code : -52, message : "stackList 정보를 입력해주세요."});
-    else if (req.body.article.stackList.length == 0)
+    else if (articleDto.stackList.length == 0)
         return res.status(200).json({code : -53, message : "stackList는 빈 배열이 될 수 없습니다."});
-    else if (!req.body.article.subjectDescription)
+    else if (!articleDto.subjectDescription)
         return res.status(200).json({code : -54, message : "subjectDescription 정보를 입력해주세요."});
-    else if (!req.body.article.projectTime)
+    else if (!articleDto.projectTime)
         return res.status(200).json({code : -55, message : "projectTime 정보를 입력해주세요."});
-    else if (!req.body.article.condition)
+    else if (!articleDto.condition)
         return res.status(200).json({code : -56, message : "condition 정보를 입력해주세요."});
-    else if (!req.body.article.progress)
+    else if (!articleDto.progress)
         return res.status(200).json({code : -57, message : "progress 정보를 입력해주세요."});
-    else if (!req.body.article.capacity)
+    else if (!articleDto.capacity)
         return res.status(200).json({code : -58, message : "capacity 정보를 입력해주세요."});
-    else if (req.body.article.capacity <= 0)
+    else if (articleDto.capacity <= 0)
         return res.status(200).json({code : -59, message : "capacity는 0이상 정수로 입력해주세요."});
     next();
 }
