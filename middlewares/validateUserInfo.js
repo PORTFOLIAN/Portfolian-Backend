9e717952b7260ba80bfa30fd0580f082e51f8297
HONGMINJU <mhong0800@gmail.com>
2022-04-19 23:53:16 +0900

    feat : 사용자 정보 수정 유효성 미들웨어 추가

---
+++ middlewares/validateUserInfo.js
@@ -0,0 +1,15 @@
+let validateUserInfo = async function(req, res, next) {
+    if (!req.body.nickName)
+        req.body.nickName = "";
+    if (!req.body.description)
+        req.body.description = ""; 
+    if (!req.body.stack)
+        req.body.stack = []; 
+    if (!req.body.github)
+        req.body.github = ""; 
+    if (!req.body.mail)
+        req.body.mail = ""; 
+    next();
+}
+
+module.exports = { validateUserInfo };
