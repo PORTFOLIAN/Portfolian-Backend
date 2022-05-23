8bb695882eff912525891fd6c6eca80b93aee1f2
HONGMINJU <mhong0800@gmail.com>
2022-05-23 18:03:08 +0900

    refactor: chat controller 삭제

--- routes/chat.js
+++ routes/chat.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const router = express.Router();
+
+// middleware
 const { validateAccessToken } = require('../middlewares/validateAccessToken');
+
+// models & service
 const User = require('../models/user');
 const Project = require('../models/project');
 const Chat = require('../models/chat');
 const ChatRoom = require('../models/chatRoom');
 const ChatService = require('../services/chat');
-    /*
+
+/*
     채팅에 관련된 Router를 정의한다.
 
     # GET  /chats              : 나의 채팅방 목록 조회
     # GET  /chats/:chatRoomId  : chatRoomId 내 채팅 메세지 조회
     # POST /chats              : 채팅방 만들기
     # PUT  /chats/:chatRoomId  : 채팅방 나가기
-    */
-
+*/
 
  //나의 채팅방 목록 조회
  router.get('/', validateAccessToken, async (req, res, next) => {
@@ -54,20 +58,6 @@ router.put('/:chatRoomId', validateAccessToken, async (req, res, next) => {
 
     const leaveRes = await chatServiceInstance.leaveChatRoom(user, chatRoomId);
     res.status(200).json(leaveRes);
-});
-
-//채팅방 나가기
-router.post('/:chatRoomId/test', validateAccessToken, async (req, res, next) => {
-    let message_data = {};
-    message_data.messageContent = req.body.message;
-    message_data.roomId = req.params.chatRoomId;
-    message_data.sender = req.user._id;
-    message_data.receiver = req.body.receiver;
-
-    console.log(`chatRoomId : ${message_data.roomId}, senderId : ${message_data.sender}, receiverId: ${message_data.receiver}`);
-    await Chat.createChat(message_data);
-    //const leaveRes = await chatServiceInstance.leaveChatRoom(user, chatRoomId);
-    res.status(200).json({"hi" : "hi"});
 });
 
 module.exports = router;
