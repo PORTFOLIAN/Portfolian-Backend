8c8fe8cfee5e6b28c11f30d5ab1068270f94a51e
HONGMINJU <mhong0800@gmail.com>
2022-05-29 18:17:03 +0900

    feat: 신고사유 빈문자열 예외처리

--- services/report.js
+++ services/report.js
@@ -9,44 +9,47 @@ class ReportService {
     }
 
     // 사용자 신고하기
-    async createUserReport(srcUsreId, destUserId, reason) {
+    async createUserReport(srcUserId, destUserId, reason) {
         const { ObjectId } = mongoose.Types;
 
-        if (!ObjectId.isValid(srcUsreId) || !ObjectId.isValid(destUserId) || !(await this.UserModel.isExistUserById(destUserId)))
+        if (!ObjectId.isValid(srcUserId) || !ObjectId.isValid(destUserId) || !(await this.UserModel.isExistUserById(destUserId)))
             return {code : -1, message : "userId가 잘못되었습니다."};
-        
-        let under24HourReport = await this.ReportModel.findUserReportUnder24Hour(srcUsreId, destUserId);
-        if (under24HourReport.length !== 0)
-            return {code : -3, message : "같은 사용자에 대해서 24시간에 한번만 신고가 가능합니다."};
-
-        if (srcUsreId === destUserId)
+        if (!reason)
+            return {code : -3, message : "신고 사유를 입력해주세요."};
+        if (srcUserId === destUserId)
             return {code : -4, message : "자신은 신고할 수 없습니다."};
 
+        let under24HourReport = await this.ReportModel.findUserReportUnder24Hour(srcUserId, destUserId);
+        if (under24HourReport.length !== 0)
+            return {code : -5, message : "같은 사용자에 대해서 24시간에 한번만 신고가 가능합니다."};
+
         //  사용자 신고 생성
-        let reportId = await this.ReportModel.createUserReport(srcUsreId, destUserId, reason);
+        let reportId = await this.ReportModel.createUserReport(srcUserId, destUserId, reason);
 
         return {code : 1, message : "사용자 신고가 성공적으로 처리되었습니다.", reportId : reportId};
 
     }
 
     // 프로젝트 신고하기
-    async createProjectReport(srcUsreId, destProjectId, reason) {
+    async createProjectReport(srcUserId, destProjectId, reason) {
         const { ObjectId } = mongoose.Types;
 
-        if (!ObjectId.isValid(srcUsreId))
+        if (!ObjectId.isValid(srcUserId))
             return {code : -1, message : "userId가 잘못되었습니다."};
         if (!ObjectId.isValid(destProjectId) || !(await this.ProjectModel.isExistProjectById(destProjectId)))
             return {code : -2, message : "projectId가 잘못되었습니다."};
-        
-        let under24HourReport = await this.ReportModel.findProjectReportUnder24Hour(srcUsreId, destProjectId);
-        if (under24HourReport.length !== 0)
-            return {code : -3, message : "같은 프로젝트에 대해서 24시간에 한번만 신고가 가능합니다."};
-
-        if ((await this.ProjectModel.isExistProjectByIdAndLeaderId(destProjectId, srcUsreId)))
+        if (!reason)
+            return {code : -3, message : "신고 사유를 입력해주세요."};
+        if ((await this.ProjectModel.isExistProjectByIdAndLeaderId(destProjectId, srcUserId)))
             return {code : -4, message : "자신의 프로젝트는 신고할 수 없습니다."};
 
+        let under24HourReport = await this.ReportModel.findProjectReportUnder24Hour(srcUserId, destProjectId);
+        if (under24HourReport.length !== 0)
+            return {code : -5, message : "같은 프로젝트에 대해서 24시간에 한번만 신고가 가능합니다."};
+
+
         //  프로젝트 신고 생성
-        let reportId = await this.ReportModel.createProjectReport(srcUsreId, destProjectId, reason);
+        let reportId = await this.ReportModel.createProjectReport(srcUserId, destProjectId, reason);
 
         return {code : 1, message : "프로젝트 신고가 성공적으로 처리되었습니다.", reportId : reportId};
     }
