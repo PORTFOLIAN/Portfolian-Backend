0bfd5bdb0c689e2d1bb9a358c10c9ee6cfd0071f
HONGMINJU <mhong0800@gmail.com>
2022-05-29 21:46:29 +0900

    fix: undefine 에러 수정

--- services/report.js
+++ services/report.js
@@ -34,8 +34,8 @@ class ReportService {
 
         //  사용자 신고 생성
         let reportId = await this.ReportModel.createUserReport(srcUserId, destUserId, reason);
-        checkUserReport(destUserId);
-        
+        this.checkUserReport(destUserId);
+
         return {code : 1, message : "사용자 신고가 성공적으로 처리되었습니다.", reportId : reportId};
     }
 
@@ -68,7 +68,7 @@ class ReportService {
 
         //  프로젝트 신고 생성
         let reportId = await this.ReportModel.createProjectReport(srcUserId, destProjectId, reason);
-        checkProjectReport(destProjectId);
+        this.checkProjectReport(destProjectId);
 
         return {code : 1, message : "프로젝트 신고가 성공적으로 처리되었습니다.", reportId : reportId};
     }
